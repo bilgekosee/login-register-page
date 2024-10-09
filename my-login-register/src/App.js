786d4a47f85ef6297d8b99077ef663f11a5c34b2
Bilge Köşe <bilgekose@Bilge-Air.home>
2024-10-09 20:02:49 +0300

    login için sayfa oluşturuldu

--- my-login-register/src/App.js
+++ my-login-register/src/App.js
@@ -2,8 +2,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      bilge
+    <div className='all-container'>
+      <header className='header-container'>
+        <div className='logo'>LOGO</div>
+        <div className='header-wrapper'>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className='login-container'>LOGIN</div>
+      </header>
+    <div className='container'>
+      
+      
+    </div>
     </div>
   );
 }
