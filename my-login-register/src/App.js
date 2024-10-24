eafac5dc1bba95d010726a04f5a7b32ebe185214
Bilge Köşe <bilgekose@Bilge-MacBook-Air.local>
2024-10-24 09:45:45 +0300

    new update register and css //TODO:responsive design

--- my-login-register/src/App.js
+++ my-login-register/src/App.js
@@ -2,15 +2,19 @@ import { React, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [showLogin, setShowLogin] = useState();
+  const [showLogin, setShowLogin] = useState(null);
 
-  const handleLoginClick = () => {
+  const handleLoginClick = (e) => {
+    e.preventDefault();
     setShowLogin(true);
     console.log("tıklandı");
   };
   const handleCloseClick = () => {
-    setShowLogin(false);
+    setShowLogin(null);
     console.log("tıklandı");
+  };
+  const handleRegisterClick = () => {
+    setShowLogin(false);
   };
   return (
     <div className="all-container">
@@ -24,20 +28,35 @@ function App() {
             <li>Contact</li>
           </ul>
         </div>
+
         <button className="login-container" onClick={handleLoginClick}>
           LOGIN
         </button>
       </header>
-      {showLogin && (
+      {showLogin === true ? (
         <div className="container">
           <div className="close-icon" onClick={handleCloseClick}>
             <div class="multiply"></div>
           </div>
           <div className="loginText">Login</div>
-          <div className="input-container">
-            <input type="text" placeholder="Email" required></input>
 
-            <input type="password" placeholder="Password" required></input>
+          <div className="input-container">
+            <i className="fas fa-envelope"></i>
+            <input
+              type="text"
+              placeholder="Email"
+              autoComplete="new-email"
+              required
+            ></input>
+          </div>
+          <div className="input-container">
+            <i className="fas fa-lock"></i>
+            <input
+              type="password"
+              placeholder="Password"
+              autoComplete="new-password"
+              required
+            ></input>
           </div>
 
           <div className="forget-container">
@@ -54,10 +73,70 @@ function App() {
           </div>
           <div className="accounted-required">
             <a>Don’t have a accounted? </a>
-            <a>Register </a>
+            <a
+              href="#register"
+              className="none-underline"
+              onClick={handleRegisterClick}
+            >
+              Register
+            </a>
           </div>
         </div>
-      )}
+      ) : showLogin === false ? (
+        <div className="container" id="register">
+          <div className="close-icon" onClick={handleCloseClick}>
+            <div class="multiply"></div>
+          </div>
+          <div className="loginText-Register">Registration</div>
+          <div className="input-container">
+            <i className="fas fa-user"></i>
+            <input
+              type="text"
+              placeholder="Name"
+              autoComplete="new-name"
+              required
+            ></input>
+          </div>
+          <div className="input-container">
+            <i className="fas fa-envelope"></i>
+            <input
+              type="text"
+              placeholder="Email"
+              autoComplete="new-email"
+              required
+            ></input>
+          </div>
+          <div className="input-container">
+            <i className="fas fa-lock"></i>
+            <input
+              type="password"
+              placeholder="Password"
+              autoComplete="new-password"
+              required
+            ></input>
+          </div>
+
+          <div className="forget-containerRegister">
+            <input type="checkbox" id="remember-checbox"></input>
+            <div className="text-container">
+              <span className="remember-spanRegister">
+                I agree to the terms & conditions
+              </span>
+            </div>
+          </div>
+          <div className="login-button">
+            <button type="button" className="loginButton">
+              Register
+            </button>
+          </div>
+          <div className="accounted-required-register">
+            <a>Already have an account? </a>
+            <a href="" className="none-underline" onClick={handleLoginClick}>
+              Login
+            </a>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
